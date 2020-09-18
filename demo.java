4e085876261077517e93c5ac0b4ad0f8e919c61f
sdendup2019 <70961659+sdendup2019@users.noreply.github.com>
2020-09-18 20:38:27 +0600

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,5 +1,7 @@
 public class MyClass {
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    int[] a={1,2,3};
+    for(int i=0;i>=2;i--){
+      System.out.println(a[i])
   }
 }
