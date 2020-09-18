43fd77b0895194d7730f08631478bc6f239a0d42
sdendup2019 <70961659+sdendup2019@users.noreply.github.com>
2020-09-18 20:39:11 +0600

    Update demo.java

--- demo.java
+++ demo.java
@@ -2,6 +2,6 @@ public class MyClass {
   public static void main(String[] args) {
     int[] a={1,2,3};
     for(int i=0;i>=2;i--){
-      System.out.println(a[i])
+      System.out.println(a[i]);
   }
 }
